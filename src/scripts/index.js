6b67633ad0450b69fb2f763f36ea8cbf97825ca8
Elizabeth Thompson <elizabeththompson@Elizabeths-MacBook-Pro.local>
2017-03-25 03:32:05 -0400

    added easing jquery function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,17 @@
 import './fullscreen';
+
+// Function to ease through the page during navigation
+$('a[href*=#]:not([href=#], a:not(.openModal)').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html, body').animate({
+                 scrollTop: target.offset().top - $('.contain-to-grid').outerHeight()
+            }, 1000);
+            return false;
+        }
+    }
+});
