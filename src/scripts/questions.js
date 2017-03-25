67e887bffc92b451d3ad2291a47546d009467c86
nickbreaton <nick@breaton.com>
2017-03-25 05:25:13 -0400

    Sequential animation

--- src/scripts/questions.js
+++ src/scripts/questions.js
@@ -20,10 +20,17 @@ $('.question input').keypress(function (e) {
   }
 });
 
+$('.next').click(function () {
+  $('.question.active').removeClass('active');
+  const next = $(this).closest('div.question').next();
+  next.addClass('active');
+  next.find('input').focus();
+  goto();
+});
+
 function goto() {
   const node = $('div.active')[0];
-  console.log(node);
-  $('#application').animate({
+  $('#application').stop().animate({
     scrollTop: node.offsetTop - (window.innerHeight / 2) + (node.offsetHeight * (1/3)),
   }, 1000);
 }
