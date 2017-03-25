c2dcbf601d503ffb1242e5da8f41117a15966a1f
nickbreaton <nick@breaton.com>
2017-03-24 22:06:19 -0400

    Add material.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 
 module.exports = {
   entry: [
-    path.resolve(__dirname, './src/index.js'),
-    path.resolve(__dirname, './src/index.scss')
+    path.resolve(__dirname, './src/scripts/index.js'),
+    path.resolve(__dirname, './src/styles/main.scss')
   ],
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -14,9 +16,16 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.scss$/,  use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.css$/,  use: ['style-loader', 'css-loader'] },
+      { test: /\.scss$/, use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
       { test: /\.js$/,   use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.json$/, use: 'json-loader' }
+      { test: /\.json$/, use: 'json-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'file-loader' }
     ]
   },
   plugins: [
@@ -30,6 +39,7 @@ module.exports = {
         removeComments: true
       },
       template: 'src/index.html'
-    })
+    }),
+    new ExtractTextPlugin('main.css')
   ]
 };
