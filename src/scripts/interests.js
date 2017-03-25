ee37a6a76223bb438cbbf8a9715a6b30613e0673
nickbreaton <nick@breaton.com>
2017-03-25 06:28:53 -0400

    Hard code list

--- src/scripts/interests.js
+++ src/scripts/interests.js
@@ -1,3 +1,14 @@
 import interests from '../data/interests.json';
 
-console.log(interests);
+for (let i = 0; i < 3; i++) {
+  $('#interests').append(`<div class="input-field col s4">`);
+}
+
+interests.sports.forEach((interest, i) => {
+  $('#interests').children().eq(i % 3).append(`
+    <p>
+      <input type="checkbox" class="filled-in" id="filled-in-box-${i}" checked="checked" />
+      <label for="filled-in-box-${i}">${interest}</label>
+    </p>
+  `);
+});
